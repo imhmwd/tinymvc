611b7fa64b9aa3e9887fb67eefc012266be6e4ea
Mohammad <mohammadjahangiri@Mohammads-MacBook-Pro.local>
2023-01-11 16:02:52 +0330

    wip

--- system/config.php
+++ system/config.php
@@ -3,7 +3,12 @@
 $base_url = "http://localhost/tinymvc/";
 $base_dir = "/tinymvc/";
 
+//c?a=a
+//[esfahanahan.com/index.php , a ]
 $tmp = explode('?', $_SERVER['REQUEST_URI']);
+
+//tmp[0] => mishe qabl az alamate soal
+//tmp[0] => esfahanahan.com/index.php
 $current_route = str_replace($base_dir, '', $tmp[0]);
 unset($tmp);
 
--- system/router/Routing.php
+++ system/router/Routing.php
@@ -7,17 +7,18 @@ use ReflectionMethod;
 class Routing
 {
     private $current_route;
-    private $method;
-    private $class;
 
     public function __construct()
     {
         global $current_route;
+        // localhost/mvc/home/index/15
         $this->current_route = explode('/', $current_route);
     }
 
     public function run()
     {
+        //check kardan inke class home vojod dare ya na
+        //check kardan inke class home vojod dare ya na
         $path = realpath(dirname(__FILE__) . "/../../application/controller/" . $this->current_route[0] . ".php");
 
         if (!file_exists($path)) {
@@ -25,17 +26,18 @@ class Routing
             exit;
         }
 
-        sizeof($this->current_route) == 1 ? $this->method == "index" : $this->method == $this->current_route[1];
+        $method = null;
+        sizeof($this->current_route) == 1 ? $method == "index" : $method == $this->current_route[1];
 
-        $this->class = "Application\contoller\\" . $this->current_route[0];
-        $object = new $this->class();
+        $class = "Application\contoller\\" . $this->current_route[0];
+        $object = new $class();
 
-        if (method_exists($object, $this->method)) {
+        if (method_exists($object, $method)) {
 
-            $reflection = new ReflectionMethod($this->class, $this->method);
+            $reflection = new ReflectionMethod($class, $method);
             $parameterCount = $reflection->getNumberOfParameters();
             if ($parameterCount <= count(array_slice($this->current_route, 2))) {
-                call_user_func_array(array($object, $this->method), array(array_slice($this->current_route, 2)));
+                call_user_func_array(array($object, $method), array(array_slice($this->current_route, 2)));
             } else {
                 echo "parameter error";
             }
