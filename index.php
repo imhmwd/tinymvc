cae01c6872dfb539c6b674353e18e6940685f0a1
Mohammad <mohammad.jahangirii79@gmail.com>
2023-01-16 11:45:03 +0330

    feat:view

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 include ('system/config.php');
 include ('system/router/Routing.php');
+include ('system/trait/Redirect.php');
+include ('system/trait/View.php');
+include ('application/controller/Controller.php');
 include ('application/controller/Home.php');
 include ('system/bootstrap/boot.php');
-
