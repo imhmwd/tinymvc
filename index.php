a481e4791e464db1d4d16eb59cc991364764ebd0
Mohammad <mohammad.jahangirii79@gmail.com>
2023-01-18 12:04:37 +0330

    feat:autoload

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
 
-include ('system/config.php');
-include ('system/router/Routing.php');
-include ('system/trait/Redirect.php');
-include ('system/trait/View.php');
-include ('application/controller/Controller.php');
-include ('application/controller/Home.php');
 include ('system/bootstrap/boot.php');
--- system/bootstrap/boot.php
+++ system/bootstrap/boot.php
@@ -1,6 +1,13 @@
 <?php
+session_start();
+use \System\Bootstrap\Autoload;
 use \System\router\Routing;
-require_once ('system/config.php');
 
-$router = new Routing();
+require_once('system/config.php');
+require_once('system/bootstrap/Autoload.php');
+
+$autoload = new Autoload;
+$autoload->autoloader();
+
+$router = new Routing;
 $router->run();
